e492c600dc476f76f0e6ad368e9f810eabc35dbf
Justin Bush <revblaze@users.noreply.github.com>
2021-06-09 09:51:24 -0400

    Create macOS sample project

---
+++ macOS/Friends/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Friends
+//
+//  Created by Justin Bush on 2021-06-09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
